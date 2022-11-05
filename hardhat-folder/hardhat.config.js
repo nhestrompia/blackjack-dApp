199332920287bf9d85c1b4390feeffd5f4f91459
nhestrompia <umutozcelik8@hotmail.com>
2022-11-05 18:55:17 +0300

    hardhat config update

--- hardhat-folder/hardhat.config.js
+++ hardhat-folder/hardhat.config.js
@@ -1,21 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config({ path: ".env" })
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const ALCHEMY_API_KEY = "QMGhIJBaoaGl3Rn6xZ9_bqezv_m94ACZ"
-
-// Replace this private key with your Goerli account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Beware: NEVER put real Ether into testing accounts
-const GOERLI_PRIVATE_KEY =
-  "a1d48106c48cd7168ff41653d398356c18de254d20fd084b0d8a908fb5da92d3"
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      // url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      url: QUICKNODE_HTTP_URL,
       accounts: [GOERLI_PRIVATE_KEY],
     },
   },
